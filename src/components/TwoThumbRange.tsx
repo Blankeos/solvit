3a23197fd92eb06a19b3b2f5168fd5dd32eb097c
Blankeos <carloantonioct@gmail.com>
2022-09-08 18:16:34 +0800

    Fixed deployment errors

--- src/components/TwoThumbRange.tsx
+++ src/components/TwoThumbRange.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { TwoThumbInputRange } from "react-two-thumb-input-range";
+// import React from "react";
+// // import { TwoThumbInputRange } from "react-two-thumb-input-range";
 
-interface TwoThumbRange {
-  values: [number, number];
-  onChange: (values: number[]) => void;
-  min: number;
-  max: number;
-}
-const TwoThumbRange: React.FC<TwoThumbRange> = ({
-  values,
-  onChange,
-  min,
-  max,
-}) => {
-  return (
-    <TwoThumbInputRange
-      trackColor="#ffffff"
-      onChange={onChange}
-      values={values}
-      min={min}
-      max={max}
-    />
-  );
-};
+// interface TwoThumbRange {
+//   values: [number, number];
+//   onChange: (values: number[]) => void;
+//   min: number;
+//   max: number;
+// }
+// const TwoThumbRange: React.FC<TwoThumbRange> = ({
+//   values,
+//   onChange,
+//   min,
+//   max,
+// }) => {
+//   return (
+//     <TwoThumbInputRange
+//       trackColor="#ffffff"
+//       onChange={onChange}
+//       values={values}
+//       min={min}
+//       max={max}
+//     />
+//   );
+// };
 
-export default TwoThumbRange;
+// export default TwoThumbRange;
